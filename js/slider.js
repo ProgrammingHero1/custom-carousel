d5b54e66605d8c0eeb02abfed364b9cb981914f1
sohanemon <m.sohanemon@gmail.com>
2022-09-15 18:54:14 +0600

    redundance removed

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,7 @@
-const images = [
-  "images/pic-1.jpg",
-  "images/pic-2.jpg",
-  "images/pic-3.jpg",
-  "images/pic-4.jpg",
-  "images/pic-5.jpg",
-  "images/pic-6.jpg",
-  "images/pic-7.jpg",
-  "images/pic-8.jpg",
-  "images/pic-9.jpg",
-];
-
-let imgIndex = 0;
-
 const img = document.getElementById("carousel-img");
 
 setInterval(() => {
-  if (imgIndex === images.length) {
-    imgIndex = 0;
-  }
-  const imgLink = images[imgIndex];
-  img.setAttribute("src", imgLink);
-  imgIndex++;
+  change(1);
 }, 4000);
 
 const change = (task) => {
